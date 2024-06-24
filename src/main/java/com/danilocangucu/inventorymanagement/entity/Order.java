dd4546121298cdceb746942c54bb1e9c0f35b5bc
Danilo Canguçu <daniloccangucu@gmail.com>
2024-06-24 16:11:13 +0300

    Order and OrderItem entities, OrderStatus enum

---
+++ src/main/java/com/danilocangucu/inventorymanagement/entity/Order.java
@@ -0,0 +1,34 @@
+package com.danilocangucu.inventorymanagement.entity;
+
+import com.danilocangucu.inventorymanagement.util.OrderStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "orders")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems;
+}
---
+++ src/main/java/com/danilocangucu/inventorymanagement/entity/OrderItem.java
@@ -0,0 +1,30 @@
+package com.danilocangucu.inventorymanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @Column(nullable = false, updatable = false)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(nullable = false)
+    private UUID productId;
+
+    @Column(nullable = false)
+    private int quantity;
+
+}
